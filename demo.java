3df581b2d6861d1e35243515df787b4187a84db7
yashureddy264-ops <yashureddy264@gmail.com>
2026-01-31 14:45:46 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,3 +1,12 @@
 public class demo {
     
 }
+public static void main(String args[]){
+    System.out.println("hello world");
+    System.out.println("mother theresa institutions");
+    System.out.println("computer science and engineering");
+}
+
+    
+
+
