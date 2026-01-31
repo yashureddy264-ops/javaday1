72cba4158d1ea497cbc721cb52047a18ff73b1b5
yashureddy264-ops <yashureddy264@gmail.com>
2026-01-31 15:59:22 +0530

    Add files via upload

---
+++ user2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class user2 {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    double a=sc.nextDouble();
+    float b=sc.nextFloat();
+    String str1=sc.next();
+    String str2=sc.nextLine();
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(str1);
+    System.out.println(age);
+    }
+}
+
+    
+       
+
